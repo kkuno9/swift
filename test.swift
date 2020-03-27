20d7993a10ec7e016b4b15c65ad831f097040713
kkuno9 <kkuno99@gmail.com>
2020-03-27 12:13:32 +0900

    test

---
+++ test.swift
@@ -0,0 +1,13 @@
+var str = "hello"
+str = "hello" + " 김형준"
+print(str)
+
+let pi:Double = 3.14
+var r:Double = 5
+var area:Double = pi * r * r
+print(area)
+
+//주어진 변수 myColor를 이용하여 I like swift 라는 문장을 print하기 위한 코드를 작성하시오.
+var language:String = "swift"
+var myColor = "I like \(language)" 
+print(myColor)
